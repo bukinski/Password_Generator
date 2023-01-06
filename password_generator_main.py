27aefa9873ab01762f4e2bfc6c6cf801a1d44824
Bukinski <gregor.szczypek@gmail.com>
2023-01-06 21:37:16 +0100

    initial working commit

---
+++ password_generator_main.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+## characters to generate password from
+charactersLowerCase = list(string.ascii_letters.lower())
+charactersUpperCase = list(string.ascii_letters.upper())
+digits = list(string.digits)
+specialCharacters = list("!@#$%^&*(),")
+
+def GeneratePW(upper, lower, digits, special):
+    PW = list()
+    PWLenghtInt = (int(input("Input desired password length: ")))
+    #print(PWLenghtInt/4)
+    for i in range (0, int(PWLenghtInt)):
+        PW.append(random.choice(upper))
+        PW.append(random.choice(lower))
+        PW.append(random.choice(digits))
+        PW.append(random.choice(special))
+    
+    #print(PW)
+
+    PW = PW[0:PWLenghtInt]
+    random.shuffle(PW)
+    print("Your random password: ")
+    print(''.join(PW))
+
+GeneratePW(charactersUpperCase, charactersLowerCase, digits, specialCharacters)
